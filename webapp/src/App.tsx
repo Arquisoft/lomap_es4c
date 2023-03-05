2a86b98cfa2bbc3fd2c28d5deb57fb08700b01d4
UO265210 <uo265210@uniovi.es>
2023-03-05 22:02:38 +0100

    Enlaces de login funcionando y cambio de ventanas

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import { SessionProvider, useSession } from "@inrupt/solid-ui-react";
 import { useState} from "react";
 import LoginForm from "./components/views/logIn"
+import Main from "./components/views/main"
 import ProfileViewer from "./components/views/profile"
+import { BrowserRouter as Router, Routes, Route }
+    from "react-router-dom";
+
 
 import "./index.css";
+function callLogin(){
+  console.log("Hola");
+  
+  
+}
 
 function App(): JSX.Element {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -21,41 +30,30 @@ function App(): JSX.Element {
   session.onLogout(()=>{
     setIsLoggedIn(false)
   })
+
+
+
   return (
+    /*
     <SessionProvider sessionId="log-in-example">
       {(!isLoggedIn) ? <LoginForm/> : <ProfileViewer/>}
     </SessionProvider>
-    /*
-    <>
-      <div className="App">
-        <header>
-          <p>LoMap</p>
-          <nav>
-            <button className="separador">log in</button>
-            <button>sign up</button>
-          </nav>
-        </header>
-        <div className="portada">
-          <img src="./images/portada.png" alt="portada" />
-          <h1>LoMap</h1>
-        </div>
-      </div>
-      <div className="description">
-        <h2> Red social de mapas</h2>
-        <p>
-          {" "}
-          seleccione el mapa que desee abrir y realice lo que usted quiera!
-          desde añadir puntos de interés como restaurantes hasta crear su propia
-          ruta y compartirla con sus amigos
-        </p>
-      </div>
-      <footer>
-        <p>Escuela Ingeniería informática 2022-2023/ASW grupo lomap_es4c</p>
-        <img src="./images/uniovi.png" alt="portada" />
-      </footer>
-    </>
     */
+    
+    <>
+      
+          <Router>
+          <Routes>
+            <Route path="/login" element={<LoginForm />}/>
+            <Route path="/profile" element={<ProfileViewer />}/>
+            <Route path="/" element={<Main />}/>
+          </Routes>
+        </Router>
+            
+    </>
+    
   );
+  
 }
 
 export default App;
