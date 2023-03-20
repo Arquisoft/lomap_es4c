8458eb619d6e41cf36d872d3ca594a79950ee24f
Alba Guerrero <uo266007@uniovi.es>
2023-03-20 21:32:56 +0100

    Login modificado,en proceso de sesion

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { SessionProvider, useSession } from "@inrupt/solid-ui-react";
-import { useState} from "react";
+import { useState,useEffect} from "react";
 import LoginForm from "./components/views/logIn"
 import Main from "./components/views/main"
 import ProfileViewer from "./components/views/profile"
 import MapViewer from "./components/views/map"
 import { BrowserRouter as Router, Routes, Route }
     from "react-router-dom";
+  import { login,handleIncomingRedirect, onSessionRestore} from "@inrupt/solid-client-authn-browser";
 
 
 import "./index.css";
@@ -15,9 +16,28 @@ function callLogin(){
   
   
 }
+/*
+const router = useRouter();
+
+// 1. Register the callback to restore the user's page after refresh and
+//    redirection from the Solid Identity Provider.
+onSessionRestore((url) => {
+  router.push(url)
+});
+
+useEffect(() => {
+  // 2. When loading the component, call `handleIncomingRedirect` to authenticate
+  //    the user if appropriate, or to restore a previous session.
+  handleIncomingRedirect({
+    restorePreviousSession: true
+  }).then((info) => {
+    console.log(`Logged in with WebID [${info.webId}]`)
+  })
+}, []);
+*/
 
 function App(): JSX.Element {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+ /* const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   //With this we can control the login status for solid
   const { session } = useSession();
@@ -31,29 +51,29 @@ function App(): JSX.Element {
   session.onLogout(()=>{
     setIsLoggedIn(false)
   })
+*/const [webId, setwebId] = useState("");
 
-
-
+  
   return (
     
-    <SessionProvider sessionId="log-in-example">
+    /*<SessionProvider sessionId="log-in-example">
       {(!isLoggedIn) ? <LoginForm/> : <MapViewer/>}
     </SessionProvider>
+    */
     
-    /*
     <>
-      
+     
           <Router>
           <Routes>
             <Route path="/login" element={<LoginForm />}/>
-            <Route path="/profile" element={<ProfileViewer />}/>
-            <Route path="/map" element={<MapViewer />}/>
+            <Route path="/profile" element={<ProfileViewer/>}/>
+            <Route path="/map" element={<MapViewer/>}/>
             <Route path="/" element={<Main />}/>
           </Routes>
         </Router>
-            
+        
     </>
-    */
+  
   );
   
 }
